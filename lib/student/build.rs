95f6262e1db160c3ab64efaa248bad71b7150b4b
Adam Chalmers <adam.s.chalmers@gmail.com>
2021-07-20 12:14:50 -0500

    More realistic example: the proto package paths do not match filesystem paths

--- lib/student/build.rs
+++ lib/student/build.rs
@@ -2,7 +2,7 @@ fn main() -> std::io::Result<()> {
     prost_build::Config::new()
         // Declare the `person.proto` Protobuf package and all
         // nested packages and types as externally provided by the `person` crate.
-        .extern_path(".person", "::person")
+        .extern_path(".deps/person", "::person")
         .compile_protos(&["../../protos/student.proto"], &["../../protos"])?;
     Ok(())
 }
